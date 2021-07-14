96c43442ce5119884efd1a75cd08e94462fcb42d
Anni <67686201+Anni-065@users.noreply.github.com>
2021-07-14 16:06:07 +0200

    Breh

--- Abgabe2/Abgabe2.c
+++ Abgabe2/Abgabe2.c
@@ -1,8 +1,8 @@
 /*
  * Aufgabe 2.c
  *
- *  Created on: xx.xx.2021
- *      Author: Mein Name
+ *  Created on: 11.05.2021
+ *      Author: Athanasia Leontarakis
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,17 +12,15 @@
  Funktion : length
 
  Beschreibung :
-	 Bestimmt die (netto) Länge der übergebenen Zeichenkette.
-	 Korrekte Funktion wird nur für 7 Bit ASCII Zeichen garantiert. UTF kodierte Zeichen führen zu fehlerhaften Längenangaben.
+	 Bestimmt die (netto) LÃ¤nge der Ã¼bergebenen Zeichenkette.
+	 Korrekte Funktion wird nur fÃ¼r 7 Bit ASCII Zeichen garantiert. UTF kodierte Zeichen fÃ¼hren zu fehlerhaften LÃ¤ngenangaben.
 
  Parameter:
-	 s - Zeichenkette dessen Länge bestimmt werden soll
+	 s - Zeichenkette dessen LÃ¤nge bestimmt werden soll
 
-
- Rückgabe:
-	 Nettolänge der Zeichenkette (ohne den Terminator)
+ RÃ¼ckgabe:
+	 NettolÃ¤nge der Zeichenkette (ohne den Terminator)
  */
-
 int length(char* s) {
 	int n = 0;
 	while (*s != '\0') {
@@ -33,10 +31,6 @@ int length(char* s) {
 	return n;
 }
 
-
-
-
-
 // Annahme: n ist > 0
 void copy(char* s, int n, char* t) {
 	int i = 0;
@@ -46,15 +40,13 @@ void copy(char* s, int n, char* t) {
 	}
 }
 
-
-
 /*
 Funktion : reverse
 
 Beschreibung :
-	Dreht die übergebene Zeichenkette zeichenweise um.
-	Der für die neue Zeichenkette benötigter Speicherplatz wird auf dem Heap reserviert.
-	Korrekte Funktion wird nur für 7 Bit ASCII Zeichen garantiert. UTF kodierte Zeichen führen zu fehlerhaften Kodierung
+	Dreht die Ã¼bergebene Zeichenkette zeichenweise um.
+	Der fÃ¼r die neue Zeichenkette benÃ¶tigter Speicherplatz wird auf dem Heap reserviert.
+	Korrekte Funktion wird nur fÃ¼r 7 Bit ASCII Zeichen garantiert. UTF kodierte Zeichen fÃ¼hren zu fehlerhaften Kodierung
 
 	Beispiele:
 	"abg" -> "gba"
@@ -64,33 +56,35 @@ Parameter:
 	s - Zeichenkette die umgedreht werden soll
 
 
-Rückgabe:
+RÃ¼ckgabe:
 	Zeiger auf die neue umgedrehte Zeichenkette
 */
+char* reverse(char* s) {
+	int n = length(s);
+	char* res = malloc(sizeof(char) * (n + 1));
+	int i = 0;
 
-char* reverse(char* s)
-{
-	// Hier sollte Ihre eigene Implementierung von reverse weilen
+	while (i < n) {
+		res[i] = s[n-i-1];
+		i++;
+	}
+	res[i] = '\0';
+	return res;
 }
-
-
 
 /*
 Funktion : putBack
 Beschreibung :
-	Fügt am Ende der übergebenen Zeichenkette (s) ein Zeichen (c) ein.
-	Der für die neue Zeichenkette benötigter Speicherplatz wird auf dem Heap reserviert.
-
+	FÃ¼gt am Ende der Ã¼bergebenen Zeichenkette (s) ein Zeichen (c) ein.
+	Der fÃ¼r die neue Zeichenkette benÃ¶tigter Speicherplatz wird auf dem Heap reserviert.
 
 Parameter:
 	s - Zeichenkette die ein neues Zeichen aufnehmen soll
-	c - Zeichen das am anfang der Zeichenkette (s) eingefügt werden soll
+	c - Zeichen das am anfang der Zeichenkette (s) eingefÃ¼gt werden soll
 
-
-Rückgabe:
-	neue, um das übergebene Zeichen erweiterte Zeichenkette.
+RÃ¼ckgabe:
+	neue, um das Ã¼bergebene Zeichen erweiterte Zeichenkette.
 */
-
 char* putBack(char* s, char c) {
 	const int n = length(s);
 	char* r = malloc(sizeof(char) * (n + 2));
@@ -103,10 +97,10 @@ char* putBack(char* s, char c) {
 /*
 Funktion : reverseRec
 Beschreibung :
-	Dreht die übergebene Zeichenkette zeichenweise um. Funktion ruft sich selbst rekursiv auf bis keine Zeichen mehr in der Übergebenen Zeichenkette vorhanden sind.
+	Dreht die Ã¼bergebene Zeichenkette zeichenweise um. Funktion ruft sich selbst rekursiv auf bis keine Zeichen mehr in der Ãœbergebenen Zeichenkette vorhanden sind.
 	Diese Funktion verwendet die Funktion putBack() um neue (gedrehte) Zeichenketten zu erstellen.
-	Alte, nicht mehr benötigte Zeichenkette der folgender Rekursionsstuffe wird mit free() freigegeben.
-	Korrekte Funktion wird nur für 7 Bit ASCII Zeichen garantiert. UTF kodierte Zeichen führen zu fehlerhaften Kodierung.
+	Alte, nicht mehr benÃ¶tigte Zeichenkette der folgender Rekursionsstuffe wird mit free() freigegeben.
+	Korrekte Funktion wird nur fÃ¼r 7 Bit ASCII Zeichen garantiert. UTF kodierte Zeichen fÃ¼hren zu fehlerhaften Kodierung.
 
 	Beispiele:
 	"abg" -> "gba"
@@ -115,27 +109,38 @@ Beschreibung :
 Parameter:
 	s - Zeichenkette die umgedreht werden soll
 
-
-Rückgabe:
+RÃ¼ckgabe:
 	Zeiger auf die neue umgedrehte Zeichenkette
 */
-char* reverseRec(char* s)
-{
-	// Hier sollte Ihre eigene Implementierung von reverseRec weilen
-	if (*s == '\0')
-	{
-		// Rekursionsabbruch
+char* reverseRec(char* s) {
+	if (*s == '\0')	{
+		char* n = malloc(sizeof(char) * 1);
+		*n = '\0';
+		return n;
 	}
-
-
+	char* n = reverseRec(++s);
+	s--;
+	s = putBack(n, *s);	
+	free(n);
+	return s;
 }
 
-int main()
-{
-	printf("Hallo Aufgabe 2!");
+int main() {
+	printf("Hallo Aufgabe 2!\n\n");
 
-	// Hier sind keine Automatischen Tests notwendig. Es reicht einfach nur ein paar umgedrehte Zeichenketten auf der Konsole auszugeben. 
+	char* test1 = "12345";
+	char* test2 = "abcde";
+	char* test3 = "a";
 
+	printf("%s reverse: %s\n", test1, reverse(test1));
+	printf("%s reverse: %s\n", test2, reverse(test2));
+	printf("%s reverse: %s\n", test3, reverse(test3));
+
+	printf("\n");
+
+	printf("%s reverseRec: %s\n", test1, reverseRec(test1));
+	printf("%s reverseRec: %s\n", test2, reverseRec(test2));
+	printf("%s reverseRec: %s\n", test3, reverseRec(test3));
 
 	return 0;
 }
